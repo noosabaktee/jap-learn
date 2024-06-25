4c71751a681562d9177dba06f0fc1685600cde79
Rama Nusa Bakti <91789732+noosabaktee@users.noreply.github.com>
2024-06-25 21:29:32 +0700

    Delete backsound

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   return (
     <div className="App">
       {/* <audio controls autoPlay loop src={require('./backsound.mp3')} style={{"display":"none"}}></audio> */}
-      <iframe src={require('./backsound.mp3')} allow="autoplay" style={{"display":"none"}} id="iframeAudio"></iframe>
+      {/* <iframe src={require('./backsound.mp3')} allow="autoplay" style={{"display":"none"}} id="iframeAudio"></iframe> */}
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/page2' element={<Page2/>} />
